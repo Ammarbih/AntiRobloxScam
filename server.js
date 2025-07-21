8ccb577c2a7c3c6ff645ed00cbc8ad16d72174bf
sibonjic2005 <1071535@hr.nl>
2025-07-21 22:42:56 +0200

    ce

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    port: parseInt(process.env.DB_PORT, 10),
 });
 
 // Test de databaseverbinding
